2d72eef22495305b9e6f702bb5cc7923df0a9bb2
Tiago Luchini <luchini@work.co>
2018-07-31 13:57:25 -0400

    Add more tests

--- test/hodur_engine/test_core.clj
+++ test/hodur_engine/test_core.clj
@@ -53,6 +53,7 @@
              t)))))
 
 (deftest test-implements
+  ;; Only one implement
   (let [res (init-and-pull
              '[Animal [species]
                Herbivore [food-preference]
@@ -62,6 +63,7 @@
              [:type/name "Cat"])]
     (is (= (-> res :type/implements first :type/name)
            "Animal")))
+  ;; More implements
   (let [res (init-and-pull
              '[Animal [species]
                Herbivore [food-preference]
@@ -77,3 +79,166 @@
                         :type/implements
                         (filter #(= (:type/name %) "Herbivore"))
                         first))))))
+
+(deftest test-type-relationships-on-field
+  ;; Only one
+  (let [res (init-and-pull
+             '[Person
+               [^Person boss]]
+             '[* {:field/_parent
+                  [* {:field/type [*]}]}]
+             [:type/name "Person"])]
+    (is (->> res
+             :field/_parent
+             (filter #(= (:field/name %) "boss"))
+             first
+             :field/type
+             :type/name)
+        "Person"))
+  ;; Specified arity
+  (let [res (init-and-pull
+             '[Person
+               [^{:type Person
+                  :arity [0 n]}
+                friends]]
+             '[* {:field/_parent
+                  [* {:field/type [*]}]}]
+             [:type/name "Person"])
+        field (->> res
+                   :field/_parent
+                   (filter #(= (:field/name %) "friends"))
+                   first)]
+    (is (-> field :field/type :type/name)
+        "Person")
+    (is (-> field :field/arity)
+        '[0 n])))
+
+(deftest test-type-relationships-on-params
+  (let [res (init-and-query
+             '[Location
+               [^Float
+                distance [^Unit unit]
+
+                ^{:type String
+                  :arity [0 n]}
+                friends [^Float
+                         distance
+                         ^{:type String
+                           :optional true}
+                         name-starting-with]]
+
+               ^{:enum true}
+               Unit [IMPERIAL METRIC]]
+             '[:find [(pull ?t [* {:field/_parent
+                                   [* {:field/type [*]
+                                       :param/_parent
+                                       [* {:param/type [*]}]}]}]) ...]
+               :where
+               [?t :type/name]])
+        location (->> res
+                      (filter #(= (:type/name %) "Location"))
+                      first)
+        distance (->> location
+                      :field/_parent
+                      (filter #(= (:field/name %) "distance"))
+                      first)
+        unit-param (->> distance
+                        :param/_parent
+                        (filter #(= (:param/name %) "unit"))
+                        first)
+        friends (->> location
+                     :field/_parent
+                     (filter #(= (:field/name %) "friends"))
+                     first)
+        distance-param (->> friends
+                            :param/_parent
+                            (filter #(= (:param/name %) "distance"))
+                            first)
+        name-param (->> friends
+                        :param/_parent
+                        (filter #(= (:param/name %) "name-starting-with"))
+                        first)]
+    (is (-> distance :field/type :type/name)
+        "Unit")
+    (is (-> distance :field/type :field/_parent count)
+        2)
+    (is (-> unit-param :param/type :type/enum)
+        true)
+    (is (-> friends :field/type :type/name)
+        "String")
+    (is (-> friends :field/arity)
+        '[0 n])
+    (is (-> friends :param/_parent count)
+        2)
+    (is (-> distance-param :param/type :type/name)
+        "Float")
+    (is (-> name-param :param/type :type/name)
+        "String")
+    (is (-> name-param :param/optional)
+        true)))
+
+(deftest test-type-markers
+  (let [res (init-and-query
+             '[^:interface InterfaceType []
+               ^:enum EnumType []
+               ^:union UnionType []
+               ^{:doc "This is the doc"
+                 :deprecation "This is the deprecation note"}
+               DocType []]
+             '[:find [(pull ?t [* {:field/_parent
+                                   [* {:field/type [*]
+                                       :param/_parent
+                                       [* {:param/type [*]}]}]}]) ...]
+               :where
+               [?t :type/name]])
+        interface (->> res
+                       (filter #(= (:type/name %) "InterfaceType"))
+                       first)
+        enum (->> res
+                  (filter #(= (:type/name %) "EnumType"))
+                  first)
+        union (->> res
+                   (filter #(= (:type/name %) "UnionType"))
+                   first)
+        doc (->> res
+                 (filter #(= (:type/name %) "DocType"))
+                 first)]
+    (is (-> interface :type/interface)
+        true)
+    (is (-> enum :type/enum)
+        true)
+    (is (-> union :type/union)
+        true)
+    (is (-> doc :type/doc)
+        "This is the doc")
+    (is (-> doc :type/deprecation)
+        "This is the deprecation note")))
+
+(deftest test-field-markers
+  (let [res (init-and-pull
+             '[Type
+               [^{:doc "This is the doc"
+                  :deprecation "This is the deprecation note"}
+                doc
+                ^{:type String
+                  :arity [4]}
+                exactly-four]]
+             '[* {:field/_parent
+                  [* {:field/type [*]
+                      :param/_parent
+                      [* {:param/type [*]}]}]}]
+             [:type/name "Type"])
+        doc (->> res
+                 :field/_parent
+                 (filter #(= (:field/name %) "doc"))
+                 first)
+        exactly-four (->> res
+                          :field/_parent
+                          (filter #(= (:field/name %) "exactly-four"))
+                          first)]
+    (is (-> doc :field/doc)
+        "This is the doc")
+    (is (-> doc :field/deprecation)
+        "This is the deprecation note")
+    (is (-> exactly-four :field/arity)
+        [4])))
