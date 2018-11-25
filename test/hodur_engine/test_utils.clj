737c45dc51adb5bcce8610d58ecade023aea873f
Tiago Luchini <luchini@work.co>
2018-11-25 18:16:30 -0500

    Add basic implementation of a topological sort

---
+++ test/hodur_engine/test_utils.clj
@@ -0,0 +1,29 @@
+(ns hodur-engine.test-utils
+  (:require [camel-snake-kebab.core :refer [->camelCaseKeyword
+                                            ->PascalCaseKeyword
+                                            ->kebab-case-keyword
+                                            ->snake_case_keyword]]
+            [clojure.test :refer :all]
+            [datascript.core :as d]
+            [hodur-engine.core :as engine]
+            [hodur-engine.utils :as utils]))
+
+(deftest ^:failing test-acyclical-topological-sort
+  (let [meta-db (engine/init-schema
+                 '[A
+                   [^String af1
+                    ^B af2
+                    ^C af3
+                    [^Integer af3p1
+                     ^C af3p2]]
+
+                   B
+                   [^C bf1]
+
+                   C
+                   [^B cf1]])
+        sorted (utils/topological-sort meta-db)]
+    (println sorted)
+    (clojure.pprint/pprint
+     (d/pull-many @meta-db '[:field/name :type/name :param/name] sorted))
+    ))
